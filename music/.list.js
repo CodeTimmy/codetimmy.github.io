7d93af01c92f7c0f770e7daa45600ab3de47d98a
Timmy_0811 <timmy.0811.mc@gmail.com>
2026-06-27 23:50:10 +0200

    New CMD Commit

---
+++ music/.list.js
@@ -0,0 +1,102 @@
+const playlist = [
+    {
+        title: "In My Head (feat. Britt Lari)",
+        artist: "Arcando",
+        file: "music/Arcando - In My Head (feat. Britt Lari).mp3"
+    },
+    {
+        title: "Everything",
+        artist: "Diamond Eyes",
+        file: "music/Diamond Eyes - Everything.mp3"
+    },
+    {
+        title: "Flutter",
+        artist: "Diamond Eyes",
+        file: "music/Diamond Eyes - Flutter.mp3"
+    },
+    {
+        title: "On My Own (feat. Karra)",
+        artist: "Far Out",
+        file: "music/Far Out - On My Own (feat. Karra).mp3"
+    },
+    {
+        title: "Dead of Night (VIP) [NCS Release]",
+        artist: "if found",
+        file: "music/if found - Dead of Night (VIP) [NCS Release].mp3"
+    },
+    {
+        title: "Moments",
+        artist: "Lost Identities x Robbie Rosen",
+        file: "music/Lost Identities x Robbie Rosen - Moments.mp3"
+    },
+    {
+        title: "If I Disappear (ft. Tom Mårtensson)",
+        artist: "Tobu",
+        file: "music/Tobu - If I Disappear (ft. Tom Mårtensson).mp3"
+    },
+    {
+        title: "Live A Lie (ft. Andreas Stone)",
+        artist: "Rival x Egzod",
+        file: "music/Rival x Egzod - Live A Lie (ft. Andreas Stone).mp3"
+    },
+    {
+        title: "Escape With Me (feat. Charlotte Haining)",
+        artist: "Halcyon & Starlyte",
+        file: "music/Halcyon & Starlyte - Escape With Me (feat. Charlotte Haining).mp3"
+    },
+    {
+        title: "I Know",
+        artist: "3rd Prototype",
+        file: "music/3rd Prototype - I Know.mp3"
+    },
+    {
+        title: "Freedom",
+        artist: "32Stitches & CHENDA, Harley Bird",
+        file: "music/32Stitches & CHENDA, Harley Bird - Freedom.mp3"
+    },
+    {
+        title: "See The Sun",
+        artist: "Abstrakt, weloveyouspydee",
+        file: "music/Abstrakt, weloveyouspydee - See The Sun.mp3"
+    },
+    {
+        title: "Where We Started (feat. Jex)",
+        artist: "Lost Sky",
+        file: "music/Lost Sky - Where We Started (feat. Jex).mp3"
+    },
+    {
+        title: "Somebody Like Me (feat. Halvorsen)",
+        artist: "JJD & Division One",
+        file: "music/JJD & Division One - Somebody Like Me (feat. Halvorsen).mp3"
+    },
+    {
+        title: "Find A Way (feat. Cammie Robinson)",
+        artist: "T & Sugah x NCT",
+        file: "music/T & Sugah x NCT - Find A Way (feat. Cammie Robinson).mp3"
+    },
+    {
+        title: "Unbreakable",
+        artist: "Lost Sky x ANGELPLAYA x Chris Linton",
+        file: "music/Lost Sky x ANGELPLAYA x Chris Linton - Unbreakable.mp3"
+    },
+    {
+        title: "Can't Heal You",
+        artist: "Bad Computer",
+        file: "music/Bad Computer - Can't Heal You.mp3"
+    },
+    {
+        title: "Warzone (feat. Zaug)",
+        artist: "Clarx",
+        file: "music/Clarx - Warzone (feat. Zaug).mp3"
+    },
+    {
+        title: "Deep End",
+        artist: "FLOTE, Fendy Cisneros, Luxtides",
+        file: "music/FLOTE, Fendy Cisneros, Luxtides - Deep End.mp3"
+    },
+    {
+        title: "Hero's Ending",
+        artist: "Lost Sky x OPALS",
+        file: "music/Lost Sky x OPALS - Hero's Ending.mp3"
+    }
+];
